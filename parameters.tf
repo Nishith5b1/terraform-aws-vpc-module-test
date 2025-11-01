6cf2d1c82795c61fdd7011bd2206108ed06a1026
Nishith5b1 <nishith5b1@gmail.com>
2025-11-02 00:06:48 +0530

    modified

--- parameters.tf
+++ parameters.tf
@@ -1,5 +1,5 @@
-# resource "aws_ssm_parameter" "vpc_id"{
-#     type = "String"
-#     name="/${var.project_name}/${var.environment}/vpc_id"
-#     value = module.vpc.vpc_id
-#     }
+resource "aws_ssm_parameter" "vpc_id"{
+    type = "String"
+    name="/${var.project_name}/${var.environment}/vpc_id"
+    value = module.vpc.vpc_id
+    }
